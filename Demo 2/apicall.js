5cbba4e298031b49360c44376e225907fc9ad77e
Rohith1299 <88533082+Rohith1299@users.noreply.github.com>
2022-09-21 10:15:33 +0530

    Add files via upload

---
+++ Demo 2/apicall.js
@@ -0,0 +1,202 @@
+ 
+$(function(){
+    var $movieDetails= $ ('#Movies') ;
+    var $movieName =$('#movieName');
+    var $Price =$('#Price');
+    var $screenNO =$('#screenNO');
+    var $theatreName =$('#theatreName');
+
+    $ .ajax({
+      type : 'GET',
+      url : 'http://localhost:8080/MovieService/webresources/movie/details' ,
+      success : function (movieDetails) {
+        console.log(movieDetails.movieDetails);
+        $.each ( movieDetails.movieDetails, function ( i ,movieDetails) {
+           console.log(movieDetails)
+          $movieDetails.append('<li>movieName: '+movieDetails.movieName+' , Price: '+movieDetails.price+',screenNo:'+movieDetails.screenNo+',theatreName:'+movieDetails.theatreName +'</li>');
+        });
+    },
+    error: function(){
+        alert('error loading Details');
+    }
+  });
+    });
+         
+    
+                                      /* Insert */
+    $('#add-Movie').on('click',function()
+
+    {      
+ 
+         Movies()
+ 
+     });    
+ 
+ function Movies()
+ {
+ 
+     var url = "http://localhost:8080/MovieService/webresources/movie/insert";
+  
+     var Movies= {};
+
+     Movies.movieName = $('#movieName').val();
+ 
+     Movies.Price = $('#Price').val();
+ 
+     Movies.screenNo = $('#screenNo').val();
+
+     Movies.theatreName = $('#theatreName').val();
+ 
+     if(Movies){
+ 
+         $.ajax({
+ 
+             url: url,
+ 
+             contentType:"application/json; charset=utf-8",
+ 
+             dataType: "json",
+ 
+            data: JSON.stringify(Movies),
+ 
+            type: "POST",
+ 
+            success: function(result){
+ 
+ 
+ 
+                
+ 
+ 
+ 
+                
+ 
+ 
+ 
+                 alert("Movie Released");
+ 
+ 
+ 
+             },
+ 
+ 
+ 
+             error: function(msg){
+ 
+ 
+ 
+                 alert(msg);
+ 
+ 
+ 
+             }
+ 
+ 
+ 
+         });
+ 
+ 
+ 
+     }
+ 
+ 
+ 
+ }
+
+
+ $('#update-Movie').on('click',function()
+
+{      
+
+     update()
+
+ });
+
+function update()
+{
+
+    var url = "http://localhost:8080/MovieService/webresources/movie/update";
+
+
+
+    var Movies = {};
+
+
+    Movies.movieName = $('#movieName').val();
+ 
+    Movies.Price = $('#Price').val();
+
+    Movies.screenNo = $('#screenNo').val();
+
+    Movies.theatreName = $('#theatreName').val();
+
+
+    if(Movies){
+
+
+
+        $.ajax({
+
+
+
+            url: url,
+
+
+
+            contentType:"application/json; charset=utf-8",
+
+
+
+            dataType: "json",
+
+
+
+            data: JSON.stringify(Movies),
+
+
+
+            type: "PUT",
+
+
+
+            success: function(result){
+
+
+
+                //getProduct();
+
+
+
+               
+
+
+
+                alert("Movie updated");
+
+
+
+            },
+
+
+
+            error: function(msg){
+
+
+
+                alert(msg);
+
+
+
+            }
+
+
+
+        });
+
+
+
+    }
+
+
+
+}
